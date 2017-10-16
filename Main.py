6c0a99c15972e998a4fb080cf408db6745f1abda
Dylan Staatz <dms9800@gmail.com>
2017-10-16 16:34:59 -0500

    Setup Main.py and TimeEntry.py

---
+++ Main.py
@@ -0,0 +1,11 @@
+''' Main.py '''
+
+import time
+
+
+def main():
+    ''' The main entry point to the application '''
+    print(time.localtime())
+
+if __name__ == "__main__":
+    main()
---
+++ TimeEntry.py
@@ -0,0 +1,19 @@
+''' TimeEntry.py '''
+
+import time
+
+
+class TimeEntry():
+    '''This class defines a single toggle time entry'''
+    
+    def __init__(project, taskName, startTime, endDateTime, duration):
+        _taskName = taskName
+        _project = project
+        _startDateTime = startDateTime
+        _endDateTime = endDateTime
+        _duration = duration
+
+    def funcname(self, parameter_list):
+        pass
+
+

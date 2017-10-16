49f4f14abafd3a730a6d782bc615baee8836aa1a
Dylan Staatz <dms9800@gmail.com>
2017-10-16 18:49:30 -0500

    Started loading data

--- Main.py
+++ Main.py
@@ -1,11 +1,33 @@
 ''' Main.py '''
 
+import csv
 import time
+import pandas as pd
 
 
 def main():
     ''' The main entry point to the application '''
-    print(time.localtime())
+
+    df = pd.DataFrame.from_csv("Toggl_time_entries_2017-08-21_to_2017-10-15.csv")
+    
+
+    with open("Toggl_time_entries_2017-08-21_to_2017-10-15.csv", 'r') as file:
+        data = csv.reader(file)
+        first_line = next(data)
+        #print(first_line)
+        d = dict()
+        d["Project"] = list()
+        d["Description"] = list()
+        d["Start"] = list()
+        d["End"] = list()
+        d["Duration"] = list()
+        first = True
+        for line in data:
+            print(line)
+
+    
+
+
 
 if __name__ == "__main__":
     main()

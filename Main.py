2c6f2772674f85c52285223148a48683cc06003c
Dylan Staatz <dms9800@gmail.com>
2017-10-17 01:41:46 -0500

    Read in dictionary to a pandas dataframe

--- Main.py
+++ Main.py
@@ -1,32 +1,69 @@
 ''' Main.py '''
 
 import csv
-import time
+from datetime import datetime, timedelta
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def read_in_file(file_path):
+    ''' Reads in a csv file to a dictionary given file_path '''
+
+    dictionary = dict()
+    with open(file_path, 'r') as file:
+        data = csv.reader(file)
+        next(data)
+        dictionary["Project"] = list()
+        dictionary["Description"] = list()
+        dictionary["Start"] = list()
+        dictionary["End"] = list()
+        dictionary["Duration"] = list()
+
+        for line in data:
+            dictionary["Project"].append(line[3])
+            dictionary["Description"].append(line[5])
+
+            start_date = line[7].split("-")
+            year = int(start_date[0])
+            month = int(start_date[1])
+            day = int(start_date[2])
+            start_time = line[8].split(":")
+            hour = int(start_time[0])
+            minute = int(start_time[1])
+            second = int(start_time[2])
+            dictionary["Start"].append(datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second))
+
+            end_date = line[9].split("-")
+            year = int(end_date[0])
+            month = int(end_date[1])
+            day = int(end_date[2])
+            end_time = line[10].split(":")
+            hour = int(end_time[0])
+            minute = int(end_time[1])
+            second = int(end_time[2])
+            dictionary["End"].append(datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second))
+
+            duration = line[11].split(":")
+            hour = int(duration[0])
+            minute = int(duration[1])
+            second = int(duration[2])
+            dictionary["Duration"].append(timedelta(hours=hour, minutes=minute, seconds=second))
+
+    return dictionary
 
 
 def main():
     ''' The main entry point to the application '''
 
-    df = pd.DataFrame.from_csv("Toggl_time_entries_2017-08-21_to_2017-10-15.csv")
+    dictionary = read_in_file("Toggl_time_entries_2017-08-21_to_2017-10-15.csv")
+    df = pd.DataFrame(dictionary)
+    # print(df.count())
+    # print(df[df['Project'] == 'Homework F17'].count()['Project'])
     
-
-    with open("Toggl_time_entries_2017-08-21_to_2017-10-15.csv", 'r') as file:
-        data = csv.reader(file)
-        first_line = next(data)
-        #print(first_line)
-        d = dict()
-        d["Project"] = list()
-        d["Description"] = list()
-        d["Start"] = list()
-        d["End"] = list()
-        d["Duration"] = list()
-        first = True
-        for line in data:
-            print(line)
-
+    df = df.groupby(by=["Project", "Description"]).sum()
     
-
+    print(df)
+    
 
 
 if __name__ == "__main__":
